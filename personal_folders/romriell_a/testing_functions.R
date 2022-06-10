a77ca3077a4820d4e405fa1a806ed4acf0f4264d
TheAdmrial <adamromriell@gmail.com>
2022-06-09 22:19:04 -0600

    more testing on dataframe

--- personal_folders/romriell_a/testing_functions.R
+++ personal_folders/romriell_a/testing_functions.R
@@ -12,20 +12,23 @@ library(rjson)
 library(dplyr)
 library(tidyr)
 library(stringr)
+
+# og_dat <- tot_employ(bea_key)
+View(og_dat)
 # getting how long it takes to run the function
 start <- Sys.time()
 dat_test <- tot_employ(bea_key)
 duration <- Sys.time() - start
 duration
 # how long does it take to get all the data from tot_employ
-# duration: 8.84 Mins
+# duration: 4 mins 33 secs
 View(dat_test)
 string_test <- dat_test$GeoName
 
 str_view_all(string_test, '[[:upper:]$]{2}')
 
 
-dat_test %>% 
+og_dat %>% 
   mutate(DataValue = str_replace_all(DataValue, ',', ''), 
          TimePeriod = as.numeric(TimePeriod), 
          DataValue = str_replace(DataValue, '\\(NA\\)', '0'),
@@ -38,7 +41,7 @@ dat_test %>%
   #          , sep = ','
   #          , extra = 'merge') %>%
   # View()
-  select(-GeoFips, -GeoName) %>%
+  select(-GeoFips, -GeoName, -NoteRef) %>%
   group_by(state, TimePeriod) %>% 
   mutate(DataValue = sum(DataValue)) %>%
   distinct() %>% 
