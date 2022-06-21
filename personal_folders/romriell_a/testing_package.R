af56053f437b8b48e1b8d88139c00b46b0abc38e
TheAdmrial <adamromriell@gmail.com>
2022-06-21 15:34:44 -0600

    added remove.package to 'update' the package to current version

--- personal_folders/romriell_a/testing_package.R
+++ personal_folders/romriell_a/testing_package.R
@@ -2,6 +2,8 @@ library(devtools)
 install_github('BYUIDSconsulting/homelessR')
 library(homelessR)
 
+# remove.packages("homelessR")
+
 
 bea_key2 <- Sys.getenv('bea_api_key')
 dat_testing <- tot_employ_bea(bea_key2)
