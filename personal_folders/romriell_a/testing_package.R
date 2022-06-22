240d106d648190a2c0d616b47902b82587274464
TheAdmrial <adamromriell@gmail.com>
2022-06-22 11:13:03 -0600

    adding the other fxn tests

--- personal_folders/romriell_a/testing_package.R
+++ personal_folders/romriell_a/testing_package.R
@@ -7,11 +7,33 @@ library(homelessR)
 
 bea_key2 <- Sys.getenv('bea_api_key')
 
+## testing Adam's fxns
+# testing total employment 
 start <- Sys.time()
 dat_testing <- tot_employ_bea(bea_key2)
 duration <- Sys.time() - start
 duration
 
+
+
+## testing Justins Fxns
+# testing HUD
+start <- Sys.time()
+dat_testing_hud <- gather_hud_data()
+duration <- Sys.time() - start
+duration
+
+View(dat_testing_hud)
+
+## testing Becca's fxns
+# testing
+start <- Sys.time()
+dat_testing_hud <- gather_hud_data()
+duration <- Sys.time() - start
+duration
+
+## testing Hunter's fxns
+# testing  
 start <- Sys.time()
 dat_testing_hud <- gather_hud_data()
 duration <- Sys.time() - start
---
+++ personal_folders/romriell_a/testing_package_depends.R
@@ -0,0 +1,6 @@
+remove.packages(c('cli','magrittr','ps','processx'))
+
+install.packages('cli')
+install.packages(c('magrittr', 'ps', 'processx'))
+
+library(cli)
