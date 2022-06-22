bdf7c3e7aa58f33040a7b267fea76a6ac5076d05
TheAdmrial <adamromriell@gmail.com>
2022-06-22 09:58:38 -0600

    adding duration test to justin's function

--- personal_folders/romriell_a/testing_package.R
+++ personal_folders/romriell_a/testing_package.R
@@ -11,3 +11,8 @@ start <- Sys.time()
 dat_testing <- tot_employ_bea(bea_key2)
 duration <- Sys.time() - start
 duration
+
+start <- Sys.time()
+dat_testing_hud <- gather_hud_data()
+duration <- Sys.time() - start
+duration
