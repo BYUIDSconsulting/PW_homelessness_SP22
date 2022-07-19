be4a02549a2e3985422ed15e3063f1a8b157a74d
bemays <may18019@byui.edu>
2022-07-19 10:38:20 -0600

    app

--- app/app.R
+++ app/app.R
@@ -18,7 +18,6 @@ full <- full_data
   ui <- fluidPage(
     sidebarLayout(
       sidebarPanel(
-<<<<<<< HEAD:app/app.R
         h3('Create your own trelliscope'),
         selectInput(inputId = 'x_axis', 
                     label = 'What column do you want for the x-axis?',
@@ -37,8 +36,6 @@ full <- full_data
         actionButton(inputId = 'trells',
                      label = 'Create Trelliscope'),
         h3('View Pre-made Trelliscopes'),
-=======
->>>>>>> 3b1b2802962638031aae313c11744846f3ef2862:app.R
         selectInput(inputId = 'trello',
                     label = 'Pre-made Trelliscopes',
                     choices = c('Violent Crime over the Years by State', 'Murder over the Years by State',
@@ -71,7 +68,6 @@ server <- function(input, output, session) {
   
   observeEvent(input$trells, {
     ## when the button is clicked, create the trelliscope in the shiny app
-    print(getwd())
     output$plot <- renderTrelliscope({
       
       # text input for formulat in facet
@@ -81,6 +77,7 @@ server <- function(input, output, session) {
       full %>%
        group_by(.data[[isolate(input$facet)]]) %>%
        nest() %>%
+      slice(1:3) %>%
        mutate(
          panel = map_plot(data, function(x) {
                             ggplot(data = x,
@@ -95,20 +92,7 @@ server <- function(input, output, session) {
        ungroup() %>%
        trelliscope(
          name = isolate(input$save_trell),
-         ncol = 2, nrow = 2, path = "www/", self_contained = TRUE)
-      
-      
-      
-      # ggplot(data = full, aes_string(x = isolate(input$x_axis), y = isolate(input$y_axis))) +
-      #   geom_line() +
-      #   geom_point() +
-      #   theme_bw() +
-      #   ylab(isolate(input$y_axis)) +
-      #   facet_trelliscope(as.formula(var), nrow = 2, ncol = 2,
-      #                     name = isolate(input$save_trell), self_contained = TRUE)
-      # 
-      # just to check.  Can be commented out when fixed.
-      cat("hello", file = "www/outfile.txt", sep = "\n", append = TRUE)
+         ncol = 2, nrow = 2, path = "www/")
       
     }) ## end of the trelliscope
     
@@ -116,42 +100,6 @@ server <- function(input, output, session) {
   
 }
 
-<<<<<<< HEAD:app/app.R
-=======
-observeEvent(input$trells, {
-## when the button is clicked, create the trelliscope in the shiny app
-
- output$plot <- renderTrelliscope({
-    input$trells
-
-  trell <- ggplot(data = full, aes_string(x = isolate(input$x_axis), y = isolate(input$y_axis))) +
-    geom_line() +
-    geom_point() +
-    theme_bw() +
-    ylab(isolate(input$y_axis)) +
-    facet_trelliscope(~ isolate(input$facet), nrow = 2, ncol = 2, name = isolate(input$save_trell), 
-                      path = '/Users/Becca/Documents/Data Consulting/homelessR/create_url/www')
-       ggsave(trell)
-  return(trell)
-#crime %>%
-#  group_by(input$facet) %>% 
-#  nest() %>%
-#  mutate(
-#    panel = map_plot(data, ~
-#                       ggplot(data = crime, aes_string(x = input$x-axis, y = input$y-axis)) +
-#                       geom_line() +
-#                       gemo_point() +
-#                       theme_bw() +
-#                       ylab(input$column)
-#    )) %>%
-#  ungroup() %>%
-#  trelliscope(name = "Crime and Homelessness", ncol = 2, nrow = 2, self_contained = TRUE)
-#)
- }) ## end of the trelliscope
-
-}) ## observeEvent for trells
-
->>>>>>> 3b1b2802962638031aae313c11744846f3ef2862:app.R
 
 # Run the application 
 shinyApp(ui = ui, server = server)
