20550b6ab41a0631d96f4399d2a01645080608da
bemays <may18019@byui.edu>
2022-07-18 20:56:54 -0600

    updated shiny app

--- app.R
+++ app.R
@@ -17,24 +17,16 @@ full <- full_data
   ui <- fluidPage(
     sidebarLayout(
       sidebarPanel(
-        selectInput(inputId = 'bea_cols', 
-                    label = 'BEA (Bureau of Economic Activity)',
-                    choices = c("State", "Number.of.CoCs", "Overall.Homeless", "Sheltered.Total.Homeless", "Unsheltered.Homeless", "Overall.Homeless.Individuals","Sheltered.Total.Homeless.Individuals", "Unsheltered.Homeless.Individuals",                                                                   
-                                "Overall.Homeless.People.in.Families", "Sheltered.Total.Homeless.People.in.Families", "Unsheltered.Homeless.People.in.Families", "Overall.Homeless.Family.Households",  "Sheltered.Total.Homeless.Family.Households", "Unsheltered.Homeless.Family.Households", "Overall.Chronically.Homeless.Individuals", "Sheltered.Total.Chronically.Homeless.Individuals", "Unsheltered.Chronically.Homeless.Individuals", "Overall.Homeless.Veterans", "Sheltered.Total.Homeless.Veterans", "Unsheltered.Homeless.Veterans", "year",                                                                   
-                                "Sheltered.Total.Chronically.Homeless", "Unsheltered.Chronically.Homeless", "Overall.Chronically.Homeless.People.in.Families",                                    
-                                "Sheltered.Total.Chronically.Homeless.People.in.Families","Unsheltered.Chronically.Homeless.People.in.Families","Overall.Homeless...Under.18", "Overall.Homeless...Age.18.to.24","Overall.Homeless...Over.24")),
-        selectInput(inputId = 'census_cols',
-                    label = 'Census',
-                    choices = c('Column 1', 'Column 2')),
-        selectInput(inputId = 'crime_cols',
-                    label = 'Crime',
-                    choices = c('state', 'violent_crime', 'murder_and_nonnegligent_manslaughter', 'rape', 'robbery', 'aggravated_assault', 'property_crime', 'burglary', 'larceny_theft', 'motor_vehicle_theft', 'arson', 'year')),
-        selectInput(inputId = 'fund_cols',
-                    label = 'Funding',
-                    choices = c('Column 1', 'Column 2')),
-        selectInput(inputId = 'hud_cols',
-                    label = 'HUD (Dept. Housing and Urban Development)',
-                    choices = c('Column 1', 'Column 2')),
+        h3('Create your own trelliscope'),
+        selectInput(inputId = 'x_axis', 
+                    label = 'What column do you want for the x-axis?',
+                    choices = colnames(full)),
+        selectInput(inputId = 'y_axis',
+                    label = 'What column do you want for the y-axis?',
+                    choices = colnames(full)),
+        selectInput(inputId = 'facet',
+                    label = 'What would you like to facet by?',
+                    choices = c('state', 'Year')),
         selectInput(inputId = 'trello',
                     label = 'Pre-made Trelliscopes',
                     choices = c('Violent Crime over the Years by State', 'Murder over the Years by State',
@@ -45,14 +37,6 @@ full <- full_data
                                 'Arson over the Years by State')),
         actionButton(inputId = 'view_trells',
                      label = "View"), ## view_trells 
-        h3('Create your own trelliscope'),
-        textInput(inputId = 'x-axis',
-                  label = 'X-Axis'),
-        textInput(inputId = 'y-axis',
-                  label = 'Y-Axis'),
-        selecInput(inputId = 'facet',
-                  label = 'Facet By',
-                  choices = c('Year', 'State')),
         textInput(inputId = 'save_trell',
                   label = 'Name to Save'),
         actionButton(inputId = 'trells',
@@ -81,12 +65,12 @@ observeEvent(input$trells, {
 
  output$plot <- renderTrelliscope({
     input$trells
-  trell <- ggplot(data = cri, aes_string(x = isolate(input$x-axis), y = isolate(input$y-axis))) +
+  trell <- ggplot(data = full, aes_string(x = isolate(input$x_axis), y = isolate(input$y_axis))) +
     geom_line() +
     geom_point() +
     theme_bw() +
-    ylab(input$column) +
-    facet_trelliscope(~ input$facet, nrow = 2, ncol = 2, name = input$save_trell, 
+    ylab(isolate(input$y_axis)) +
+    facet_trelliscope(~ isolate(input$facet), nrow = 2, ncol = 2, name = isolate(input$save_trell), 
                       path = '/Users/Becca/Documents/Data Consulting/homelessR/create_url/www')
        ggsave(trell)
  trell
